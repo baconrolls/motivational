fd75fe5e4448b7b650b53686b9c2f34a510ecd4e
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 20:28:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,29 +19,11 @@ function showNextQuestion() {
   document.getElementById('question' + currentQuestion).style.display = 'none';
   currentQuestion++;
 
-  if (currentQuestion <= 2) {
+  if (currentQuestion <= 10) {
     document.getElementById('question' + currentQuestion).style.display = 'block';
   } else {
     showResult();
   }
 }
 
-function showResult() {
-  document.getElementById('loading').style.display = 'block';
-  setTimeout(() => {
-    document.getElementById('loading').style.display = 'none';
-
-    let resultText = '';
-
-    if (scoreA >= scoreB && scoreA >= scoreC) {
-      resultText = 'You are a Beach Resort Lover!';
-    } else if (scoreB >= scoreA && scoreB >= scoreC) {
-      resultText = 'You are an Adventure Seeker in the Mountains!';
-    } else {
-      resultText = 'You are a Cultural City Explorer!';
-    }
-
-    document.getElementById('resultText').innerText = resultText;
-    document.getElementById('result').style.display = 'block';
-  }, 2000);
-}
+function showResult
