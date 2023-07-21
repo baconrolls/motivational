f93cf1f04e54951b40bbfa9daad0e2a63bc813eb
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 22:50:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function revealAnswer(card) {
-  let answer;
-  switch (card) {
-    case 'card1':
-      answer = "You should try yoga to relax and destress.";
-      break;
-    case 'card2':
-      answer = "Go for a walk in nature to rejuvenate your mind.";
-      break;
-    // Add more cases for other cards here
-    default:
-      answer = "Select a card to reveal the positive activity.";
-      break;
-  }
-  document.getElementById('result').textContent = answer;
+function showCard(selectedCard) {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+    if (index === selectedCard - 1) {
+      card.classList.add('active');
+    } else {
+      card.classList.remove('active');
+    }
+  });
 }
