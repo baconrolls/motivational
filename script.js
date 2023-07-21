5a0b51eb1c1d7b8e097f77ec0ed060315b0463e0
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 21:08:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-let currentQuestion = 1;
-let answers = [];
-
-function showQuestion(questionNumber) {
-  document.querySelectorAll('.question').forEach((question) => {
-    question.style.display = 'none';
-  });
-
-  document.querySelector(`#question${questionNumber}`).style.display = 'block';
+function revealCard(cardIndex) {
+  const cards = [
+    "Practice gratitude and write down three things you are grateful for today.",
+    "Take a walk in nature and enjoy the fresh air and scenery.",
+    "Call or video chat with a friend or family member and catch up.",
+    // Add more positive activities here
+  ];
+  const resultText = document.getElementById("resultText");
+  resultText.textContent = cards[cardIndex];
 }
-
-function nextQuestion(answer) {
-  answers.push(answer);
-
-  if (currentQuestion < 10) {
-    currentQuestion++;
-    showQuestion(currentQuestion);
-  } else {
-    showResult();
-  }
-}
-
-function showResult() {
-  document.getElementById('loading').style.display = 'block';
-
-  setTimeout(() => {
-    const resultText = document.getElementById('resultText');
-    const totalA = answers.filter((answer) => answer === 'A').length;
-    const totalB = answers.filter((answer) => answer === 'B').length;
-    const totalC = answers.filter((answer) => answer === 'C').length;
-
-    if (totalA > totalB && totalA > totalC) {
-      resultText.innerText = 'Your Skincare Personality: You have Dry Skin!';
-    } else if (totalB > totalA && totalB > totalC) {
-      resultText.innerText = 'Your Skincare Personality: You have Oily Skin!';
-    } else {
-      resultText.innerText = 'Your Skincare Personality: You have Combination Skin!';
-    }
-
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('result').style.display = 'block';
-  }, 2000);
-}
-
-showQuestion(currentQuestion);
