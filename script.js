92c18f487ff9f68f7a65350b90a629e8d445ce11
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 21:11:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function revealCard(cardIndex) {
     "Practice gratitude and write down three things you are grateful for today.",
     "Take a walk in nature and enjoy the fresh air and scenery.",
     "Call or video chat with a friend or family member and catch up.",
+    "Try a new recipe and cook a delicious meal for yourself or your loved ones.",
+    "Do a random act of kindness for someone else, like paying for someone's coffee or holding the door open.",
+   
     // Add more positive activities here
   ];
   const resultText = document.getElementById("resultText");
