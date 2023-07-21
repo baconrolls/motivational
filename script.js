e9be5d535b5782165be8303ba8f330d843a892f2
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-21 21:17:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function revealCard(cardIndex) {
-  const cards = [
-    "Practice gratitude and write down three things you are grateful for today.",
-    "Take a walk in nature and enjoy the fresh air and scenery.",
-    "Call or video chat with a friend or family member and catch up.",
-    "Try a new recipe and cook a delicious meal for yourself or your loved ones.",
-    "Do a random act of kindness for someone else, like paying for someone's coffee or holding the door open.",
-   
-    // Add more positive activities here
-  ];
-  const resultText = document.getElementById("resultText");
-  resultText.textContent = cards[cardIndex];
+function revealCard(cardNumber) {
+  // Replace the cardNumber with the actual content or activity for each card.
+  let positiveActivities = {
+    1: "Go for a nature walk and appreciate the beauty around you.",
+    2: "Try a new hobby or activity that interests you.",
+    // Add more positive activities for each card number.
+  };
+
+  let resultText = positiveActivities[cardNumber];
+  if (resultText) {
+    window.location.href = `result.html?activity=${encodeURIComponent(resultText)}`;
+  }
 }
