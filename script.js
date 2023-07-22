88c409eaf767cf36ac466ceb732456420bdb9e9b
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-22 21:02:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,42 +29,12 @@
   "Celebrate your strengths and embrace your flaws as part of who you are.",
 ];
 
-
-    // Function to shuffle the cards randomly
-    function shuffleCards() {
-      const cardsContainer = document.querySelector(".card-container");
-      const cards = Array.from(cardsContainer.children);
-
-      for (let i = cards.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [cards[i], cards[j]] = [cards[j], cards[i]];
-      }
-
-      cards.forEach((card) => {
-        cardsContainer.appendChild(card);
-      });
-    }
-
-    // Call the function to shuffle the cards when the page loads
-    window.addEventListener("load", shuffleCards);
-
-    function showCard(cardNumber) {
+function showResult(cardNumber) {
       const resultContainer = document.getElementById("resultContainer");
       const resultMessage = document.getElementById("resultMessage");
 
-      // Hide all cards
-      const cards = document.querySelectorAll(".card");
-      cards.forEach((card) => {
-        card.style.display = "none";
-      });
-
-      // Show the selected card
-      const selectedCard = document.querySelector(`.card:nth-child(${cardNumber})`);
-      selectedCard.style.display = "block";
-
-      // Show a random message on the selected card
-      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-      resultMessage.textContent = randomMessage;
+      // Show the result message based on the selected card
+      resultMessage.textContent = messages[cardNumber];
 
       // Show the result container
       resultContainer.style.display = "block";
@@ -72,13 +42,5 @@
 
     function backToMain() {
       const resultContainer = document.getElementById("resultContainer");
-      const cards = document.querySelectorAll(".card");
-
-      // Hide the result container
       resultContainer.style.display = "none";
-
-      // Show all cards again
-      cards.forEach((card) => {
-        card.style.display = "block";
-      });
     }
