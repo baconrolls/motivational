6d7f995797d346375fee064762aaf8695249d40b
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-22 13:17:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// JavaScript
+// script.js
+
 const cards = document.querySelectorAll('.card');
 const resultContainer = document.querySelector('.result-container');
 const selectedCard = document.getElementById('selectedCard');
 const resultMessage = document.getElementById('result-text');
-const resultHeading = document.getElementById('result-heading');
 const indexTitle = document.getElementById('index-title');
 
 function shuffleCards() {
@@ -23,32 +23,32 @@ function revealCard(cardNumber) {
   resultContainer.style.display = 'flex';
 
   // Display the selected card in the result container
-  selectedCard.innerHTML = cards[cardNumber - 1].innerHTML;
+  selectedCard.innerHTML = cards[cardNumber - 1].outerHTML;
 
   // Determine the result based on the card number
   let result;
   if (cardNumber === 1) {
-    resultHeading.textContent = "Practice Gratitude";
-    result = "Take a few moments each day to reflect on the things you are grateful for. Keeping a gratitude journal or simply expressing appreciation for the little things can help shift your focus to the positive aspects of your life.";
+    result = "Practice Gratitude: Take a few moments each day to reflect on the things you are grateful for. Keeping a gratitude journal or simply expressing appreciation for the little things can help shift your focus to the positive aspects of your life.";
   } else if (cardNumber === 2) {
-    resultHeading.textContent = "Spread Kindness";
-    result = "Perform random acts of kindness for others, whether it's helping a stranger, complimenting a friend, or volunteering for a cause you care about. Kindness not only benefits others but also brings joy to your own heart.";
+    result = "Spread Kindness: Perform random acts of kindness for others, whether it's helping a stranger, complimenting a friend, or volunteering for a cause you care about. Kindness not only benefits others but also brings joy to your own heart.";
   } else if (cardNumber === 3) {
-    resultHeading.textContent = "Stay Active";
-    result = "Engage in regular physical activity that you enjoy, whether it's going for a walk, dancing, or playing a sport. Exercise releases endorphins, which can boost your mood and overall well-being.";
+    result = "Stay Active: Engage in regular physical activity that you enjoy, whether it's going for a walk, dancing, or playing a sport. Exercise releases endorphins, which can boost your mood and overall well-being.";
   } else if (cardNumber === 4) {
-    resultHeading.textContent = "Connect with Loved Ones";
-    result = "Spend quality time with family and friends, either in person or through virtual means. Building and maintaining strong relationships can provide emotional support and a sense of belonging.";
+    result = "Connect with Loved Ones: Spend quality time with family and friends, either in person or through virtual means. Building and maintaining strong relationships can provide emotional support and a sense of belonging.";
   } else if (cardNumber === 5) {
-    resultHeading.textContent = "Practice Mindfulness";
-    result = "Take time to be present in the moment and practice mindfulness. Whether it's through meditation, deep breathing, or mindful activities like coloring or gardening, being mindful can reduce stress and increase feelings of calmness.";
+    result = "Practice Mindfulness: Take time to be present in the moment and practice mindfulness. Whether it's through meditation, deep breathing, or mindful activities like coloring or gardening, being mindful can reduce stress and increase feelings of calmness.";
   } else if (cardNumber === 6) {
-    resultHeading.textContent = "Pursue Your Passions";
-    result = "Engage in activities that bring you joy and fulfillment. Whether it's pursuing a hobby, learning something new, or working towards a personal goal, following your passions can lead to a greater sense of purpose and satisfaction.";
+    result = "Pursue Your Passions: Engage in activities that bring you joy and fulfillment. Whether it's pursuing a hobby, learning something new, or working towards a personal goal, following your passions can lead to a greater sense of purpose and satisfaction.";
   }
 
   // Display the result message
   resultMessage.textContent = result;
+}
+
+function showDeck() {
+  // Hide the result container and show the deck again
+  document.getElementById('deck').style.display = 'flex';
+  resultContainer.style.display = 'none';
 }
 
 // Shuffle the cards when the page loads
@@ -63,8 +63,8 @@ cards.forEach((card, index) => {
     // Add the "selected" class to the clicked card
     card.classList.add('selected');
 
-    // Show the result and hide the index title, and pass the selected card to the revealCard function
-    revealCard(index);
+    // Show the result and hide the index title, and pass the selected card to the showResult function
+    revealCard(index + 1);
   });
 });
 
