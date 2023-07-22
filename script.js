5abee173ff7591b42fae596e791fe438455536b3
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-22 21:09:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,12 +29,44 @@
   "Celebrate your strengths and embrace your flaws as part of who you are.",
 ];
 
-function showResult(cardNumber) {
+
+    // Function to shuffle the cards randomly
+    function shuffleCards() {
+      const cardsContainer = document.querySelector(".card-container");
+      const cards = Array.from(cardsContainer.children);
+
+      for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+      }
+
+      cards.forEach((card) => {
+        cardsContainer.appendChild(card);
+      });
+    }
+
+    // Call the function to shuffle the cards when the page loads
+    window.addEventListener("load", shuffleCards);
+
+    function showCard(cardNumber) {
       const resultContainer = document.getElementById("resultContainer");
       const resultMessage = document.getElementById("resultMessage");
 
-      // Show the result message based on the selected card
-      resultMessage.textContent = messages[cardNumber];
+      // Hide all cards
+      const cards = document.querySelectorAll(".card");
+      cards.forEach((card) => {
+        card.style.display = "none";
+          // Remove the onclick event from the card images
+        card.removeAttribute("onclick");
+      });
+
+      // Show the selected card
+      const selectedCard = document.querySelector(`.card:nth-child(${cardNumber})`);
+      selectedCard.style.display = "block";
+
+      // Show a random message on the selected card
+      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+      resultMessage.textContent = randomMessage;
 
       // Show the result container
       resultContainer.style.display = "block";
@@ -42,5 +74,13 @@ function showResult(cardNumber) {
 
     function backToMain() {
       const resultContainer = document.getElementById("resultContainer");
+      const cards = document.querySelectorAll(".card");
+
+      // Hide the result container
       resultContainer.style.display = "none";
+
+      // Show all cards again
+      cards.forEach((card) => {
+        card.style.display = "block";
+      });
     }
