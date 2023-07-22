8e2d96ae3e32a89ed024bcd1664722fe4f5a57d9
baconrolls <140130015+baconrolls@users.noreply.github.com>
2023-07-22 20:37:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-function showCard(cardNumber) {
-  const resultContainer = document.getElementById("resultContainer");
-  const resultMessage = document.getElementById("resultMessage");
-  const backButton = document.querySelector('.back-button');
 
-  // Hide all cards
-  const cards = document.querySelectorAll(".card");
-  cards.forEach((card) => {
-    card.style.display = "none";
-  });
+    // Array of random messages for each card
+    const messages = [
+      "You are adventurous and seeking new experiences!",
+      "You are a creative and imaginative person!",
+      "You are wise and have a strong intuition!",
+      // Add more messages here as needed
+    ];
 
-  // Show the selected card
-  const selectedCard = document.querySelector(`.card:nth-child(${cardNumber})`);
-  selectedCard.style.display = "block";
-
-  // Show the result message based on the selected card
-  switch (cardNumber) {
-    case 1:
-      resultMessage.textContent = "You are adventurous and seeking new experiences!";
-      break;
-    case 2:
-      resultMessage.textContent = "You are a creative and imaginative person!";
-      break;
-    case 3:
-      resultMessage.textContent = "You are wise and have a strong intuition!";
-      break;
-    default:
-      resultMessage.textContent = "Select a card to reveal your result!";
-  }
-
-  // Show the result container
-  resultContainer.style.display = "block";
-}
-function backToMain() {
-  const resultContainer = document.getElementById("resultContainer");
-  const cards = document.querySelectorAll(".card");
-
-  // Hide the "Back to Main" button on the main page
-  document.getElementById("back-button").style.display = "none";
-// Show the "Back to Main" button on the result page
-  document.getElementById("back-button").style.display = "block";
-
-   // Function to shuffle the cards randomly
+    // Function to shuffle the cards randomly
     function shuffleCards() {
       const cardsContainer = document.querySelector(".card-container");
-      for (let i = cardsContainer.children.length; i >= 0; i--) {
-        cardsContainer.appendChild(cardsContainer.children[Math.random() * i | 0]);
+      const cards = Array.from(cardsContainer.children);
+
+      for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
       }
+
+      cards.forEach((card) => {
+        cardsContainer.appendChild(card);
+      });
     }
 
     // Call the function to shuffle the cards when the page loads
     window.addEventListener("load", shuffleCards);
 
-  // Hide the result container
-  resultContainer.style.display = "none";
+    function showCard(cardNumber) {
+      const resultContainer = document.getElementById("resultContainer");
+      const resultMessage = document.getElementById("resultMessage");
 
-  // Show all cards again
-  cards.forEach((card) => {
-    card.style.display = "block";
-  });
-}
+      // Hide all cards
+      const cards = document.querySelectorAll(".card");
+      cards.forEach((card) => {
+        card.style.display = "none";
+      });
+
+      // Show the selected card
+      const selectedCard = document.querySelector(`.card:nth-child(${cardNumber})`);
+      selectedCard.style.display = "block";
+
+      // Show a random message on the selected card
+      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+      resultMessage.textContent = randomMessage;
+
+      // Show the result container
+      resultContainer.style.display = "block";
+    }
+
+    function backToMain() {
+      const resultContainer = document.getElementById("resultContainer");
+      const cards = document.querySelectorAll(".card");
+
+      // Hide the result container
+      resultContainer.style.display = "none";
+
+      // Show all cards again
+      cards.forEach((card) => {
+        card.style.display = "block";
+      });
+    }
